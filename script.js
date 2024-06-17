ac232c6d70eed65f8e4e64c116f0e6c35d07e888
babik234 <babik234@seznam.cz>
2024-06-17 09:54:55 +0200

    mobile update

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function changeImg() {
     console.log("DAS");
     if (x == 1) {
         icon.src = "imgs/X.png";
-        document.getElementById("nav").style.height = 40 + "vh";
+        document.getElementById("nav").style.height = 410 + "px";
         document.getElementById("a").style.display = "flex";
         x = 0;
     } else {
         icon.src = "imgs/Hamburger_icon.svg.png";
         document.getElementById("a").style.display = "none";
-        document.getElementById("nav").style.height = 10 + "vh";
+        document.getElementById("nav").style.height = 100 + "px";
         x++;
     }
 }
