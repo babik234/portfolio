e7265de9df5590afebf6421a04141fafcc32a834
babik234 <babik234@seznam.cz>
2024-06-19 13:40:05 +0200

    idk

--- moviesScript.js
+++ moviesScript.js
@@ -2,28 +2,41 @@ const proxyUrl = "https://api.allorigins.win/raw?url=";
 let apiUrl = "https://www.freetogame.com/api/games";
 
 let gamesGrid = document.getElementById("games-grid");
-search();
+let genreButtons = document.getElementById("genre-buttons");
+let loading = document.getElementById("loading");
 
+let gamesData = null;
+let genreArray = [];
+
+renderData();
+async function renderData() {
+    gamesData = await getGameData();
+    gamesData.forEach((item) => {
+        if (!genreArray.includes(item.genre)) {
+            genreArray.push(item.genre);
+        }
+
+        renderItem(item);
+    });
+
+    genreArray.forEach((genre) => {
+        renderGenreButton(genre);
+    });
+
+    loading.style.display = "none";
+}
 async function search() {
     try {
         const data = await getGameData();
 
-        if (data) {
+        if (data.length > 1) {
             console.log("Game Data:", data);
 
             data.forEach((item) => {
-                let itemDiv = document.createElement("div");
-                itemDiv.className = "game-item";
-                itemDiv.innerHTML = `
-
-                   <img src="${item.thumbnail}" alt="" width="300" height="300">
-                <div class="description">
-                    <div>Title: ${item.title}</div>
-                    <div>Genre: ${item.genre}</div>
-                </div>
-                `;
-                gamesGrid.appendChild(itemDiv);
+                renderItem(item);
             });
+        } else {
+            renderItem(data);
         }
     } catch (error) {
         console.error("Error fetching game data:", error);
@@ -46,53 +59,105 @@ async function getGameData() {
     }
 }
 
-async function searchOne() {
-    try {
-        const data = await getGameData();
-
-        if (data) {
-            console.log("Game Data:", data);
-
-            let itemDiv = document.createElement("div");
-            itemDiv.className = "game-item";
-            itemDiv.innerHTML = `
-
-                   <img src="${data.thumbnail}" alt="" width="300" height="300">
-                <div class="description">
-                    <div>Title: ${data.title}</div>
-                    <div>Genre: ${data.genre}</div>
-                </div>
-                `;
-            gamesGrid.appendChild(itemDiv);
-        }
-    } catch (error) {
-        console.error("Error fetching game data:", error);
-    }
-}
-
 function PC() {
-    removeChild();
-    apiUrl = " https://www.freetogame.com/api/games?platform=pc";
-    getGameData();
-    search();
+    itemDivs = document.querySelectorAll(".game-item");
+    itemDivs.forEach((item) => {
+        if (item.innerHTML.includes("PC")) {
+        } else {
+            item.style.display = "none";
+        }
+    });
 }
 
 function RESET() {
-    removeChild();
-    apiUrl = "https://www.freetogame.com/api/games";
-    getGameData();
-    search();
+    itemDivs = document.querySelectorAll(".game-item");
+    itemDivs.forEach((item) => {
+        item.style.display = "grid";
+    });
+    genreClicked = true;
+    genreSort();
 }
 
 function SEARCH() {
-    let id = document.getElementById("input").value;
-    removeChild();
-    apiUrl = "https://www.freetogame.com/api/game?id=" + id;
-    getGameData();
-    searchOne();
+    let value = document.getElementById("input").value;
+    value = value.toUpperCase();
+    itemDivs = document.querySelectorAll(".game-item");
+    itemDivs.forEach((item) => {
+        let titleDiv = item.querySelector(".title").textContent.toUpperCase();
+        if (titleDiv.includes(`${value}`)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
 }
 
 function removeChild() {
     let el = gamesGrid;
     while (el.firstChild) el.removeChild(el.firstChild);
 }
+
+function renderItem(item) {
+    let itemDiv = document.createElement("div");
+    itemDiv.setAttribute("data-genre", item.genre);
+    itemDiv.setAttribute("data-platform", item.platform);
+    itemDiv.className = "game-item";
+    itemDiv.innerHTML = `
+    <div class="grid">
+        <img src="${item.thumbnail}" alt="" width="400" height="300">
+    
+            <div class="title"> ${item.title.toUpperCase()}</div>
+        <div class="description">
+            <div>Genre: ${item.genre}</div>
+            <div>platform: ${item.platform}</div>
+        </div>
+     <div class="hover"></div>
+    </div>
+    `;
+    gamesGrid.appendChild(itemDiv);
+    itemDiv.addEventListener("mouseover", () => {
+        console.log("AD");
+    });
+}
+
+function renderGenreButton(genre) {
+    let button = document.createElement("button");
+    button.setAttribute("data-genre", genre);
+    button.className = "genre-button";
+    button.innerHTML = genre;
+
+    genreButtons.appendChild(button);
+
+    button.addEventListener("click", () => {
+        let buttonActive = button.classList.contains("active");
+        let items = gamesGrid.querySelectorAll(`.game-item`);
+
+        if (buttonActive) {
+            let selectedItems = gamesGrid.querySelectorAll(`[data-status="${genre}"]`);
+
+            items.forEach((item) => {
+                let itemGenre = item.getAttribute("data-genre");
+
+                if (genre == itemGenre) {
+                    item.style.display = "";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+        } else {
+            items.forEach((item) => {
+                let itemGenre = item.getAttribute("data-genre");
+
+                if (genre == itemGenre) {
+                    item.style.display = "";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+
+            button.classList.add("active");
+        }
+    });
+}
+
+document.getElementById("platform-pc").addEventListener("click", PC);
